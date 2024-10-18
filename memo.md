01a0bb10bde36d63837558a82361f95bad561f66
rameeeee <moderato611@naver.com>
2024-10-18 23:53:32 +0900

    [241018] 백엔드 배포 파일 내용 추가

---
+++ memo.md
@@ -0,0 +1,6 @@
+# 배포
+
+1. 데이터베이스 배포 : Mongodb Altas
+2. 백엔드 배포 : aws beanstalk
+3. 프론트엔드 배포 : netlify
+4. 테스트
--- todo-be/app.js
+++ todo-be/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+require('dotenv').config()
 
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+// console.log('MONGODB_URI_PROD', MONGODB_URI_PROD)
 
 const app = express();
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/api', indexRouter)
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose.connect(mongoURI, {useNewUrlParser:true}).then(() => {
     console.log('mongoose connected');
@@ -16,6 +21,6 @@ mongoose.connect(mongoURI, {useNewUrlParser:true}).then(() => {
     console.log("DB connected fail", err)
 })
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("server on 5000")
 })
